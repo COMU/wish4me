a23c2de72c8b9b95fb8dbd2d588bea8d5f4683cc
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2011-11-02 13:07:25 +0200

    user is authenticated via login() logout() functions from the Django module

--- oguz/wish4meUI/foursq_auth/views.py
+++ oguz/wish4meUI/foursq_auth/views.py
@@ -10,6 +10,8 @@ from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.contrib.auth import logout
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 CLIENT_ID = '1OVOPIR5HS5XEXJYNB4B1QNCPLFLIVULYCGHT3BFSNCG5HMR'
 CLIENT_SECRET = 'JKYYZB5FIDQEHIE3MB4VZARVWWTEZTN1ICOAK1IPFBCHSSQH'
@@ -34,7 +36,7 @@ def unauth(request):
     # clear any tokens and logout
     request.session.clear()
     logout(request)
-    return HttpResponseRedirect(reverse('main'))
+    return HttpResponseRedirect(reverse('main_view'))
 
 def callback(request):
    # get the code returned from foursquare
@@ -73,7 +75,23 @@ def done( request ):
     response = urllib2.urlopen(full_url)
     response = response.read()
     user = json.loads(response)['response']['user']
-    name = user['firstName']
-
-    # show the page with the user's name to show they've logged in
-    return render_to_response('foursq_auth/done.html', {'name':name})
+    name = " ".join([user['firstName'], user['']])
+    contact = user['contact']
+    email = contact['email']
+    obj, created = User.objects.get_or_create(email, email)
+    if not created:
+        # create the user with the username and email as his/her email
+        passwd = User.objects.make_random_password()
+        obj.set_password(passwd)
+        obj.save()
+    user_obj = User.objects.get(email=email)
+    user = authenticate(username=email, password=user_obj.password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # show the page with the user's name to show they've logged in
+            return render_to_response('foursq_auth/done.html', {'name':name})
+        else:
+            return render_to_response('errors/disabled_account.html', {'name', name})
+    else:
+        return render_to_response('errors/invalid_login', {'name', name})
