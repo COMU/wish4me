f9be7d4664b23e6dfcbf83c8e85e2f697d48583e
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2011-11-03 11:57:45 +0200

    wrong foursq app

--- wish4meUI/foursq/foursq/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Foursq_User(models.Model):
-    foursq_id = models.IntegerField()
-    user = models.ForeignKey(User)
--- wish4meUI/settings.py
+++ wish4meUI/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_PATH, 'foursq_oauth.db'),                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_PATH, 'wish4me.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
