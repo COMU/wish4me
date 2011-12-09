2e10bbbf72291736b6cd00f8c47147c98cd3c380
Orcun Avsar <orc.avs@gmail.com>
2011-12-09 17:17:40 +0200

    Forgot that import, shit happens.

--- wish4meUI/urls.py
+++ wish4meUI/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
