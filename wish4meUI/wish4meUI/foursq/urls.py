38d948111f3f69eac7aa80a94e4f9b0405890c65
Oguz Yarimtepe <oguzyarimtepe@gmail.com>
2011-12-23 18:40:14 +0200

    fix for view implementation

--- wish4meUI/wish4meUI/foursq/urls.py
+++ wish4meUI/wish4meUI/foursq/urls.py
@@ -5,13 +5,13 @@ urlpatterns = patterns('foursq.views',
     # main page redirects to start or login
     url(r'^$', view=main, name='main_view'),
     # receive OAuth token from 4sq
-    url(r'^callback/$', view=callback, name='oauth_return'),
+    url(r'^callback/$', view='callback', name='oauth_return'),
     # logout from the app
-    url(r'^logout/$', view=unauth, name='oauth_unauth'),
+    url(r'^logout/$', view='unauth', name='oauth_unauth'),
     # authenticate with 4sq using OAuth
-    url(r'^auth/$', view=auth, name='oauth_auth'),
+    url(r'^auth/$', view='auth', name='oauth_auth'),
     # main page once logged in
-    url( r'^done/$', view=done, name='oauth_done' ),
+    url( r'^done/$', view='done', name='oauth_done' ),
     # friend import page
-    url( r'^friend_import/$', view=friend_import, name='friend_import' ),
+    url( r'^friend_import/$', view='friend_import', name='friend_import' ),
 )
