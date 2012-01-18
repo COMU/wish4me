0c4dd7ef8b896929f52bc2975f251285df330650
Orcun Avsar <orc.avs@gmail.com>
2012-01-18 17:16:43 +0200

    New design, every request object now has getCurrentProfile method.

--- wish4meUI/wish4meUI/google/auth.py
+++ wish4meUI/wish4meUI/google/auth.py
@@ -1,11 +1,15 @@
-#! -*- coding : utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from openid.consumer.consumer import SUCCESS
 
 from django.contrib.auth.models import User
-from openid.consumer.consumer import SUCCESS
 from django.core.mail import mail_admins
 
+from wish4meUI.google.models import GoogleProfile
 
-class GoogleBackend:
+
+class GoogleAuthBackend(object):
 
   def authenticate(self, openid_response):
 
@@ -21,16 +25,25 @@ class GoogleBackend:
     google_lastname = openid_response.getSigned(
         'http://openid.net/srv/ax/1.0', 'value.lastname')
     try:
-      user = User.objects.get(username=google_email)
-    except User.DoesNotExist:
-      # create a new user, or send a message to admins, etc.
+      google_profile = GoogleProfile.objects.get(email=email)
+      google_profile.last_login_backend_name = 'google'
+      google_profile.save()
+      user = google_profile.user_profile.user
+    except GoogleProfile.DoesNotExist:
       user = User(username=google_email, email=google_email)
       user.save()
-
+      user_profile = UserProfile(
+          user=user, last_login_backend_name='google')
+      user_profile.save()
+      google_profile = GoogleProfile(
+          user_profile=user_profile,
+          email=email,
+          firstname=google_firstname,
+          lastname=google_lastname)
+      google_profile.save()
     return user
 
   def get_user(self, user_id):
-
     try:
       return User.objects.get(pk=user_id)
     except User.DoesNotExist:
--- wish4meUI/wish4meUI/google/models.py
+++ wish4meUI/wish4meUI/google/models.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+from wish4meUI.userprofile.models import UserProfile
+
+
+class GoogleProfile(models.Model):
+  user_profile = models.ForeignKey(UserProfile)
+  email = models.CharField(max_length=100)
+  firstname = models.CharField(max_length=100)
+  lastname = models.CharField(max_length=100)
+
+  def __unicode__(self):
+    return self.email
