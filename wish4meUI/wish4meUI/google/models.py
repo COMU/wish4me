8d13773468a8e8a241b9cf104f46aed6ef2edd03
Orcun Avsar <orc.avs@gmail.com>
2012-01-19 15:14:23 +0200

    Login profiles should be related with User instead of UserProfile.

--- wish4meUI/wish4meUI/google/models.py
+++ wish4meUI/wish4meUI/google/models.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from django.contrib.auth.models import User
 from django.db import models
-
-from wish4meUI.userprofile.models import UserProfile
 
 
 class GoogleProfile(models.Model):
-  user_profile = models.ForeignKey(UserProfile)
+  user = models.ForeignKey(User)
   email = models.CharField(max_length=100)
   firstname = models.CharField(max_length=100)
   lastname = models.CharField(max_length=100)
