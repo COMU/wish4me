3dd97f1b5c6191157eeede9dd5c4265ea84a53b8
Mesutcan Kurt <mesutcank@gmail.com>
2012-01-23 17:11:21 +0200

    fix wish home url.

--- wish4meUI/wish4meUI/wish/urls.py
+++ wish4meUI/wish4meUI/wish/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('',
-    url(r'$', view=homeWish, name="home"),
+    url(r'^$', view=homeWish, name="home"),
 #    url(r'^add_wish$', view=addWish, name="addWish"),
     #url(r'^list_wish-(?P<wish_category_id>\d+)/$', view=listWish),
     url(r'^list_wish-(?P<wishlist_id>\d+)/$', view=listWish, name='list_wish'),
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^list_wish_category$', view=listWishCategory),
 
     url(r'^list_wishlist$', view=listWishlist),
-    url(r'^add_wishlist-(?P<wishlist_id>\d+)/$', view=addWishlist, name="addWishlist")
+    url(r'^add_wishlist$', view=addWishlist, name="addWishlist")
 
 
 )
