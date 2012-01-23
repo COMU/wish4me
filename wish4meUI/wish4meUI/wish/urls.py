944b4914624b9251003cfc10424d451794bc8423
Mesutcan Kurt <mesutcank@gmail.com>
2012-01-23 17:14:47 +0200

    add all urls' name parameter.

--- wish4meUI/wish4meUI/wish/urls.py
+++ wish4meUI/wish4meUI/wish/urls.py
@@ -9,9 +9,9 @@ urlpatterns = patterns('',
     #url(r'^list_wish-(?P<wish_category_id>\d+)/$', view=listWish),
     url(r'^list_wish-(?P<wishlist_id>\d+)/$', view=listWish, name='list_wish'),
 #    url(r'^add_wish_category$', view=addWishCategory, name="addWishCategory"),
-    url(r'^list_wish_category$', view=listWishCategory),
+    url(r'^list_wish_category$', view=listWishCategory, name="listWishCategory"),
 
-    url(r'^list_wishlist$', view=listWishlist),
+    url(r'^list_wishlist$', view=listWishlist, name="listWishlist"),
     url(r'^add_wishlist$', view=addWishlist, name="addWishlist")
 
 
