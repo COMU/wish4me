29597feaeaf6853fb8861d2f8e28c698343b8280
Mesutcan Kurt <mesutcank@gmail.com>
2012-01-24 10:40:20 +0200

    fix add_wish url.

--- wish4meUI/wish4meUI/wish/urls.py
+++ wish4meUI/wish4meUI/wish/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', view=homeWish, name="home"),
-    url(r'^add_wish-(?P<wishlist_id>\d+/$', view=addWish, name="addWish"),
+    url(r'^add_wish-(?P<wishlist_id>\d+)/$', view=addWish, name="addWish"),
     #url(r'^list_wish-(?P<wish_category_id>\d+)/$', view=listWish),
     url(r'^list_wish-(?P<wishlist_id>\d+)/$', view=listWish, name='list_wish'),
 #    url(r'^add_wish_category$', view=addWishCategory, name="addWishCategory"),
