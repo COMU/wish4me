4fe57ec130632cbfca128f9caba41854818d6d88
Engin Manap <enginmanap@gmail.com>
2012-01-24 13:49:36 +0200

    Change welcome page to show users wishes, if logged in.

--- wish4meUI/wish4meUI/main/views.py
+++ wish4meUI/wish4meUI/main/views.py
@@ -3,8 +3,12 @@
 
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def welcome(request):
-  return render_to_response("home/welcome.html",
+  if request.user.is_authenticated():
+    return HttpResponseRedirect(reverse("listAllWishes"))
+  else:
+    return render_to_response("home/welcome.html",
                             context_instance=RequestContext(request, {}))
