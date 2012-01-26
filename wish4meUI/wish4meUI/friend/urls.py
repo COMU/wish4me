51d227769182ed40334c3df9deede487bf06a1a2
Engin Manap <enginmanap@gmail.com>
2012-01-26 18:21:41 +0200

    Add follower listing to profile.
    the right side from layout2 was moved to _profile.html.
    a new page, view and url added for follower listing.

--- wish4meUI/wish4meUI/friend/urls.py
+++ wish4meUI/wish4meUI/friend/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('friend.views',
     url(r'^follow/(?P<following_user_id>\d+)$',  view='follow',      name='friend_follow'),
+    url(r'^list_followers$',  view='listFollowers',      name='friend_followers'),
 )
--- wish4meUI/wish4meUI/friend/views.py
+++ wish4meUI/wish4meUI/friend/views.py
@@ -3,18 +3,41 @@ from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
 
 from friend.models import *
 
 @login_required
-def follow(request,following_user_id):
+def follow(request, following_user_id):
   user_to_follow = User.objects.get(pk = following_user_id)
   user_following = request.user;
   if user_to_follow == user_following:
     print "you cannot follow yourself"
-  elif Following.objects.filter(from_user=user_following, to_user=user_to_follow).count() > 0:
+  elif Following.objects.filter(from_user=user_following, to_user=user_to_follow, is_hidden = False).count() > 0:
     print "You are following that user already"
   else:
     following = Following(from_user=user_following, to_user=user_to_follow)
     following.save()
   return HttpResponseRedirect(reverse("homePage"))
+
+@login_required
+def listFollowers(request):
+  following_user = request.user
+  followers = Following.objects.filter(to_user=following_user, is_hidden = False)
+  followers_list = []
+  for follower in followers:
+    follower_dict = dict()
+    follower_dict["username"] = follower.from_user.username
+    friendship_invite = FriendshipInvitation.objects.filter(from_user = follower.from_user, to_user=following_user, status = "1")
+    if friendship_invite is not None:
+      follower_dict["invite_id"] = friendship_invite[0].id      #TODO this is a little hackish. fix with try/catch
+    else:
+      followers_dict[follower.from_user.username] = "-1"
+    followers_list.append(follower_dict)
+
+  return render_to_response('friend/followers.html', {'followers_list': followers_list}, context_instance=RequestContext(request))
+
+       
+    
+    
+  
