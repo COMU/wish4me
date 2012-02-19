4e0243f26149211855e3fe9f67b0c9983959770d
Orcun Avsar <orc.avs@gmail.com>
2012-02-19 04:53:12 +0200

    Add welcome page with bs design.
    This commit contains first 'create_list' usage. See 'templates/home/welcome.html'.

--- wish4meUI/wish4meUI/urls.py
+++ wish4meUI/wish4meUI/urls.py
@@ -11,7 +11,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
-    url(r'^$', view='wish4meUI.main.views.welcome', name='homePage'),
+    url(r'^$', view='wish4meUI.main.views.welcome', name='welcome_page'),
     # (r'^wish4meUI/', include('wish4meUI.foo.urls')),
     (r'^foursq/', include('wish4meUI.foursq.urls')),
     (r'^twitter/', include('wish4meUI.twitter_app.urls')),
