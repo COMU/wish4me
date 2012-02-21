9d11fece38c87bcc61bca3c6ebccaf259b3b7067
Engin Manap <enginmanap@gmail.com>
2012-02-21 23:04:26 +0200

    Fix deleted wishes is not listed any more.

--- wish4meUI/wish4meUI/main/views.py
+++ wish4meUI/wish4meUI/main/views.py
@@ -16,7 +16,7 @@ from django.db import connection
 def list_friends_wishes(request):
   following_list = Following.objects.filter(from_user = request.user).values('to_user_id')
   #print(str(Wish.objects.filter(related_list__owner__in = following_list).query))
-  wishes = Wish.objects.filter(related_list__owner__in = following_list).order_by("-request_date")[:5]
+  wishes = Wish.objects.filter(related_list__owner__in = following_list, is_hidden = False).order_by("-request_date")[:5]
   return render_to_response("home/list_friend_wishes.html", {'wish_list' : wishes, }, context_instance=RequestContext(request, {}))
 
 def welcome(request):
