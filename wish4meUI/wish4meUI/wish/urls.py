090ad7c84fa2e9b08f066518b6f49b238be5a560
Mesutcan Kurt <mesutcank@gmail.com>
2012-02-22 10:29:37 +0200

    add wish_show_wish_alone url and add comments url

--- wish4meUI/wish4meUI/wish/urls.py
+++ wish4meUI/wish4meUI/wish/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^$', view=homeWish, name="wish_home"),
     url(r'^wish/add/(?P<wishlist_id>\d+)$', view=addWish, name="wish_add_wish"),
+    url(r'^wish/show/(?P<wish_id>\d+)$', view=showWishAlone, name="wish_show_wish_alone"),
     url(r'^wish/edit/(?P<wish_id>\d+)$', view=editWish, name="wish_edit_wish"),
     url(r'^wish/remove/(?P<wish_id>\d+)$', view=removeWish, name="wish_remove_wish"),
     url(r'^wish/accomplish/(?P<wish_id>\d+)$', view=accomplishWish, name="wish_accomplish_wish"),
@@ -19,7 +20,8 @@ urlpatterns = patterns('',
     url(r'^wishlist/edit/(?P<wishlist_id>\d+)$', view=editWishlist, name="wish_edit_wishlist"),
     url(r'^wishlist/remove/(?P<wishlist_id>\d+)$', view=removeWishlist, name="wish_remove_wishlist"),
     url(r'^wish/list/all$', view=listAllWishes, name="wish_list_allwishes"),
-    url(r'^wishlist/add$', view=addWishlist, name="wish_add_wishlist")
+    url(r'^wishlist/add$', view=addWishlist, name="wish_add_wishlist"),
+    (r'^comments/', include('django.contrib.comments.urls')),
 
 
 )
