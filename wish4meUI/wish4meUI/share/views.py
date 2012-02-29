7f6e6d778397e008dff42ccc115b627d2cc0bd29
Faruk Uzun <farukuzun@mail.com>
2012-03-01 00:56:01 +0200

    Add tweet via share and fixed fb

--- wish4meUI/wish4meUI/share/views.py
+++ wish4meUI/wish4meUI/share/views.py
@@ -1,34 +1,39 @@
-import cgi, urllib, json, urllib2
+import cgi, urllib, json, urllib2, oauth, httplib
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from twitter_app.utils import *
 
 def home(request):
     print "home"
     return HttpResponseRedirect('/')
 
 def facebook(request):
-username = request.GET.get('username', '')
-fb_id = request.GET.get('fb_id', '')
-fb_message = request.GET.get('fb_message', '')
-fb_token = request.GET.get('fb_access_token', '')
-fb_name = 'follow {}'.format(username)
-fb_link = 'http://wish4me.com/{}'.format(username)
-         actions = ({
-             'name' : fb_name,
-             'link' : fb_link,
-         })
-if fb_id and fb_message and fb_token:
-url = 'https://graph.facebook.com/me/feed'
-values = {'message' : fb_message,
-         'access_token' : fb_token,
-'actions' : actions,
-          }
-data = urllib.urlencode(values)
-req = urllib2.Request(url, data)
-response = urllib2.urlopen(req)
-     print "success"
-return HttpResponseRedirect('/')
+    username = request.GET.get('username', '')
+    id = request.GET.get('id', '')
+    message = request.GET.get('message', '')
+    token = request.GET.get('access_token', '')
+    name = 'follow {}'.format(username)
+    link = 'http://wish4me.com/{}'.format(username)
+    actions = ({
+             'name' : name,
+             'link' : link,
+              })
+    if id and message and token:
+    	url = 'https://graph.facebook.com/id/feed'
+    	values = {'message' : message,
+        	     'access_token' : token,
+        	     'actions' : actions,
+        	 }
+    	data = urllib.urlencode(values)
+    	req = urllib2.Request(url, data)
+    	response = urllib2.urlopen(req)
+    return HttpResponseRedirect('/')
 
 def twitter(request):
-    print "twitter"
+    CONSUMER = oauth.OAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET)
+    CONNECTION = httplib.HTTPSConnection(SERVER)
+    message = request.GET.get('message', '')
+    access_token = request.session.get('access_token', None)
+    token = oauth.OAuthToken.from_string(access_token)
+    update_status(CONSUMER, CONNECTION, token, message)
     return HttpResponseRedirect('/')
