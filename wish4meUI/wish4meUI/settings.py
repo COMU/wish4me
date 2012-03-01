3357604bf00d68ea139913f50a72d09d234f3326
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-03-01 17:17:47 +0200

    custom context processor is added to retrieve the default image at the tempate

--- wish4meUI/wish4meUI/settings.py
+++ wish4meUI/wish4meUI/settings.py
@@ -98,6 +98,16 @@ TEMPLATE_DIRS = (
     #os.path.join(PROJECT_PATH, 'templates'),
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "userprofile.context_processors.default_profile_image",
+)
+
+
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
