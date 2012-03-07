ebdc41a37b8028ab25d25b8b411f39678b24e4df
Faruk Uzun <farukuzun@mail.com>
2012-03-07 15:57:17 +0200

    add share with open graph

--- wish4meUI/wish4meUI/share/urls.py
+++ wish4meUI/wish4meUI/share/urls.py
@@ -3,6 +3,5 @@ from share.views import *
 
 urlpatterns = patterns('',
     url(r'^$', view=home),
-    url(r'^facebook$', view=facebook),
-    url(r'^twitter$', view=twitter),
+    url(r'^show/(?P<wish_id>\d+)$', view=show, name="wish_show_wish_alone"),
 )
--- wish4meUI/wish4meUI/share/views.py
+++ wish4meUI/wish4meUI/share/views.py
@@ -1,39 +1,11 @@
-import cgi, urllib, json, urllib2, oauth, httplib
-from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect
-from twitter_app.utils import *
+from wish4meUI.wish.models import Wish
 
 def home(request):
-    print "home"
     return HttpResponseRedirect('/')
 
-def facebook(request):
-    username = request.GET.get('username', '')
-    id = request.GET.get('id', '')
-    message = request.GET.get('message', '')
-    token = request.GET.get('access_token', '')
-    name = 'follow {}'.format(username)
-    link = 'http://wish4me.com/{}'.format(username)
-    actions = ({
-             'name' : name,
-             'link' : link,
-              })
-    if id and message and token:
-    	url = 'https://graph.facebook.com/id/feed'
-    	values = {'message' : message,
-        	     'access_token' : token,
-        	     'actions' : actions,
-        	 }
-    	data = urllib.urlencode(values)
-    	req = urllib2.Request(url, data)
-    	response = urllib2.urlopen(req)
-    return HttpResponseRedirect('/')
-
-def twitter(request):
-    CONSUMER = oauth.OAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET)
-    CONNECTION = httplib.HTTPSConnection(SERVER)
-    message = request.GET.get('message', '')
-    access_token = request.session.get('access_token', None)
-    token = oauth.OAuthToken.from_string(access_token)
-    update_status(CONSUMER, CONNECTION, token, message)
-    return HttpResponseRedirect('/')
+def show(request, wish_id):
+   wish = get_object_or_404(Wish, pk=wish_id)
+   return render_to_response('share/show.html', {'wish': wish}, context_instance=RequestContext(request))
