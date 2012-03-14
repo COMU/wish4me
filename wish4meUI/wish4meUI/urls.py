840fb88f4c45e52a6cd95e93a42e2426ce2b5015
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-03-14 11:31:13 +0200

    commenting is added for the activity page
    missing url for comments are added also

--- wish4meUI/wish4meUI/urls.py
+++ wish4meUI/wish4meUI/urls.py
@@ -24,6 +24,7 @@ urlpatterns = patterns('',
     (r'^user/', include('wish4meUI.userprofile.urls')),
     (r'^friend/', include('wish4meUI.friend.urls')),
     (r'^share/', include('wish4meUI.share.urls')),
+    (r'^comments/', include('django.contrib.comments.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
