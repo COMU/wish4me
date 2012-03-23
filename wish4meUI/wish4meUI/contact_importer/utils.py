816227719623f2f31e0d139f12baccf392964727
Faruk Uzun <farukuzun@mail.com>
2012-03-23 15:30:44 +0200

    Add twitter and foursq import contacts feature

--- wish4meUI/wish4meUI/contact_importer/utils.py
+++ wish4meUI/wish4meUI/contact_importer/utils.py
@@ -1,7 +1,18 @@
-import urllib, urllib2, httplib, re
+import oauth, urllib, urllib2, httplib, re
 from django.shortcuts import render_to_response
 from facebook.models import FacebookProfile
-
+from twitter_app.utils import *
+from foursq.models import FoursqProfile
+try:
+    import simplejson
+except ImportError:
+    try:
+        import json as simplejson
+    except ImportError:
+        try:
+            from django.utils import simplejson
+        except:
+            raise "Requires either simplejson, Python 2.6 or django.utils!"
 
 def facebook_contact_import(request):
 
@@ -17,8 +28,32 @@ def facebook_contact_import(request):
     full_url = url + '?' + url_values
     response = urllib2.urlopen(full_url)
     response = response.read()
-    x = re.findall(r'"\d+"',response)
-    friend_list = [a.strip('"') for a in x]
-    #friends_count = len(friend_list) #not really needed, but may come in handy
-    return friend_list
+    return response
 
+def twitter_contact_import(request):
+    CONSUMER = oauth.OAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET)
+    CONNECTION = httplib.HTTPSConnection(SERVER)
+    access_token = request.session.get('access_token', None)
+    token = oauth.OAuthToken.from_string(access_token)
+    friend_list = get_friends(CONSUMER, CONNECTION, token, page=0)
+    return friend_list
+    
+def google_contact_import(request):
+    print request.session.get('access_token')
+    
+    
+def foursquare_contact_import(request):
+    user = request.user
+    profile = user.get_profile()
+    foursq_id = profile.foursq_profile_id
+    token = FoursqProfile.objects.get(id=foursq_id)
+    token = token.access_token
+    values = {}
+    values['oauth_token'] = token
+    url_values = urllib.urlencode(values)
+    url = 'https://api.foursquare.com/v2/users/self/friends'
+    full_url = url + '?' + url_values
+    response = urllib2.urlopen(full_url)
+    response = response.read()
+    return response
+    
