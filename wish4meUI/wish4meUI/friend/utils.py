4bbf090cfef22132d9d4e1d01a21a7924ebb0f46
Mesutcan Kurt <mesutcank@gmail.com>
2012-03-28 20:46:15 +0300

    show common people count at search page: fixes #60

--- wish4meUI/wish4meUI/friend/utils.py
+++ wish4meUI/wish4meUI/friend/utils.py
@@ -10,3 +10,56 @@ def getFollowingWishes(request):
   wishes_from_following = Wish.objects.filter(related_list__owner__in = following_list, is_hidden = False, is_private = False)
   wishes = wishes_from_friends | wishes_from_following
   return wishes
+
+def getCommonFriendCount(request, friend_id):
+  user_follower = Following.objects.filter(from_user = request.user, is_hidden = False)
+  user_followed = Following.objects.filter(to_user = request.user, is_hidden = False)
+
+  _this_friendship = Following.objects.filter(from_user = request.user, to_user = friend_id, is_hidden = False)
+
+  if len(_this_friendship) == 0:
+    _this_friendship = Following.objects.filter(from_user = friend_id, to_user = request.user, is_hidden = False)
+
+  if len(_this_friendship) != 0:
+    this_friendship = _this_friendship[0]
+
+  user_followers = user_follower | user_followed
+
+  if len(_this_friendship) != 0:
+    user_followers = user_followers.exclude(pk = this_friendship.id)
+
+  friend_follower = Following.objects.filter(from_user = friend_id, is_hidden = False)
+  friend_followed = Following.objects.filter(to_user = friend_id, is_hidden = False)
+
+  friend_followers = friend_follower | friend_followed
+
+  if len(_this_friendship) != 0:
+    friend_followers = friend_followers.exclude(pk = this_friendship.id)
+
+  common_friends = []
+
+
+  for user_follow in user_followers:
+    if user_follow.from_user == request.user:
+      _user = user_follow.to_user
+    else:
+      _user = user_follow.from_user
+
+    for friend_follow in friend_followers:
+      if friend_follow.from_user == friend_id:
+        _friend = friend_follow.to_user
+      else:
+        _friend = friend_follow.from_user
+      if user_follow.from_user.get_profile().is_private == True:
+        if user_follow.to_user == request.user and _user == _friend:
+          common_friends.append(user_follow)
+      if _user == _friend:
+        common_friends.append(user_follow)
+
+  return len(common_friends)
+
+        
+
+    
+  
+  
