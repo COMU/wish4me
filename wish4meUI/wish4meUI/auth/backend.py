988e156c4d3bf98758cbcd5df485782d5ad35683
Orcun Avsar <orc.avs@gmail.com>
2012-03-29 21:10:08 +0300

    Allow using multiple social logins for one account.

--- wish4meUI/wish4meUI/auth/backend.py
+++ wish4meUI/wish4meUI/auth/backend.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import login as djangoLogin
 
@@ -37,6 +38,10 @@ class LoginBackend(object):
 
     def login(self, profile, related_name, **user_kwargs):
 
+      if self._request.user and self._request.user.is_authenticated():
+        already_logged_user = self._request.user
+      else:
+        already_logged_user = None
 
       try:
           #if related_name == "foursq_profile":
@@ -45,16 +50,30 @@ class LoginBackend(object):
           #    print profile
           userprofile = profile.getUserProfile()
           user = userprofile.user
+          if already_logged_user:
+            messages.add_message(self._request, messages.ERROR,
+                                 "This external account has already "
+                                 "been used before. Merging profiles "
+                                 "is not yet possible! Switched to "
+                                 "this account now!")
       except UserProfile.DoesNotExist:
-          user = User(**user_kwargs)
-          user.password = settings.DEFAULT_PASSWORD
-          user.save()
-          kwargs = {related_name: profile}
-          userprofile = UserProfile(user=user, **kwargs)
-          userprofile.last_login_backend_name = related_name
-          userprofile.save()
-          addDefaultWishlist(user)
-          
+          if already_logged_user:
+            user = already_logged_user
+            userprofile = user.get_profile()
+            setattr(userprofile, related_name, profile)
+            userprofile.save()
+            messages.add_message(self._request, messages.SUCCESS,
+                                 "Activated new external account!")
+          else:
+            user = User(**user_kwargs)
+            user.password = settings.DEFAULT_PASSWORD
+            user.save()
+
+            kwargs = {related_name: profile}
+            userprofile = UserProfile(user=user, **kwargs)
+            userprofile.last_login_backend_name = related_name
+            userprofile.save()
+            addDefaultWishlist(user)
       #djangoLogin(self._request,user)
 
-      return userprofile.user
+      return user
