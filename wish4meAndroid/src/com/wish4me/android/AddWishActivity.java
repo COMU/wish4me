77d34a6e7c2d174a4e9f185e8f258b4b0f9face2
Engin Manap <enginmanap@gmail.com>
2012-05-13 17:17:19 +0300

    remove unused import

--- wish4meAndroid/src/com/wish4me/android/AddWishActivity.java
+++ wish4meAndroid/src/com/wish4me/android/AddWishActivity.java
@@ -22,7 +22,7 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
